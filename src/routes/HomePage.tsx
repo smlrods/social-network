c307cc1713bc29e4e29eec538cb09354420ac0cc
sml <smlrodrigues.dev@gmail.com>
2023-06-06 11:04:28 -0300

    Add HomePage route

---
+++ src/routes/HomePage.tsx
@@ -0,0 +1,24 @@
+import { Container, Grid } from "@mui/material";
+import { useOutletContext } from "react-router-dom";
+import SideLeftBar from "../components/SideLeftBar";
+import SideRightBar from "../components/SideRightBar";
+import Posts from "../components/Posts";
+import usePosts from "../hooks/usePosts";
+
+const HomePage = () => {
+  const user = useOutletContext();
+
+  return (
+    <Container sx={{ marginY: '50px' }}>
+      <Grid container >
+        <SideLeftBar user={user} />
+        <Grid item xs={6}>
+          <Posts user={user} useQuery={usePosts} />
+        </Grid>
+        <SideRightBar />
+      </Grid>
+    </Container>
+  );
+};
+
+export default HomePage;
