8b0f0c951d618976af4f924a8e713940a781f272
sml <smlrodrigues.dev@gmail.com>
2023-06-07 08:42:43 -0300

    Add guest login

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,8 +1,27 @@
 import { Box, Button, Container, Typography } from "@mui/material";
-import { useOutletContext } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate, useOutletContext, useRevalidator } from "react-router-dom";
+import api from "../api";
+import { LoadingButton } from "@mui/lab";
 
 const Index = () => {
   const user = useOutletContext();
+  const revalidator = useRevalidator();
+  const navigate = useNavigate();
+
+  const mutation = useMutation({
+    mutationFn: (user: { username: string, password: string }) => {
+      return api.auth.login(user);
+    },
+    onSuccess: () => {
+      revalidator.revalidate();
+      navigate('/home');
+    }
+  });
+
+  const handleGuestLogin = () => {
+    mutation.mutate({ username: 'guest', password: '123' });
+  }
 
   return (
     <Container
@@ -20,17 +39,34 @@ const Index = () => {
           fontFamily: 'monospace',
           fontWeight: '700',
           letterSpacing: '.9rem',
+          wordSpacing: '0.1rem',
         }}
+      >
+        Social Network
+      </Typography>
+      <Typography
+        variant="body1"
         gutterBottom
       >
-        Welcome
+        Connect with friends and the world around you on Social Network.
       </Typography>
       <Box>
         {user ?
           <Button variant="outlined" size="large" href="/home">Go to Homepage</Button> :
           <>
-            <Button sx={{ marginX: 1 }} variant="outlined" size="large" href="/login">Log in</Button>
-            <Button sx={{ marginX: 1 }} variant="outlined" size="large" href="/signup">Sign up</Button>
+            <Button sx={{ margin: 1 }} variant="contained" size="large" href="/login">Log in</Button>
+            <Button sx={{ margin: 1 }} variant="contained" size="large" href="/signup">Sign up</Button>
+            <Box>
+              <LoadingButton
+                variant="outlined"
+                size="small"
+                fullWidth
+                loading={mutation.isLoading}
+                onClick={handleGuestLogin}
+              >
+                Guest Login
+              </LoadingButton>
+            </Box>
           </>
         }
       </Box>
